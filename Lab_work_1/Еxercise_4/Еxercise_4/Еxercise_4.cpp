fd429b20a023052da8ae83a05b1722d001fdd97b
Star_killer <68467056+Star-Kuller@users.noreply.github.com>
2022-09-18 14:11:08 +0300

    3

---
+++ Lab_work_1/Еxercise_4/Еxercise_4/Еxercise_4.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int pound;
+    int shilling;
+    int penny;
+    cout << "Введите количество фунтов: "; cin >> pound;
+    cout << "Введите количество шиллингов: "; cin >> shilling;
+    cout << "Введите количество пенсов: "; cin >> penny;
+    penny = penny + (shilling * 12);
+    cout << "Десятичных фунтов: " << round((float)(pound + ( ((float)penny / 12) / 20 ))*100)/100 << "\n \n";
+    return 0;
+}
