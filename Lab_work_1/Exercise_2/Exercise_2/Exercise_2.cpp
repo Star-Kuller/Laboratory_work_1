49484e39ac95c1468366fe00010e2fc21377b9c5
Star_killer <68467056+Star-Kuller@users.noreply.github.com>
2022-09-18 09:32:39 +0300

    0

---
+++ Lab_work_1/Exercise_2/Exercise_2/Exercise_2.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double t;
+    cout << "Введите температуру в градусах цельсия: "; cin >> t; cout << endl;
+    cout << "Температура по Фаренгейту: " << (t * 9 / 5) + 32 << "\n \n";
+    return 0;
+}
