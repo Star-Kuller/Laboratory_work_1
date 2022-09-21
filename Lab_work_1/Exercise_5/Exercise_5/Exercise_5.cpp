997df48cb9f1de3ea05d2d8dd92b4375bad9b898
Star_killer <68467056+Star-Kuller@users.noreply.github.com>
2022-09-21 14:23:52 +0300

    5

---
+++ Lab_work_1/Exercise_5/Exercise_5/Exercise_5.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    float pound;
+    float shilling;
+    int penny;
+    cout << "Введите число десятичных фунтов: "; cin >> pound;
+    shilling = pound * 20;
+    penny = shilling * 12;
+    shilling = (int)(penny / 12);
+    penny = penny - shilling * 12;
+    pound = (int)(shilling / 20);
+    shilling = shilling - pound * 20;
+    cout << "Эквивалентная сумма в старой форме записи: " << (int)pound << "." << (int)shilling << "." << penny << "\n \n";
+    return 0;
+}
