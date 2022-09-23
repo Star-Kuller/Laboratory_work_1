8ffdba7affd9df67352defea6a33d99e393f6241
Star_killer <68467056+Star-Kuller@users.noreply.github.com>
2022-09-23 13:00:02 +0300

    7

--- Lab_work_1/Exercise_5/Exercise_5/Exercise_5.cpp
+++ Lab_work_1/Exercise_5/Exercise_5/Exercise_5.cpp
@@ -1,20 +1,24 @@
 ﻿#include <iostream>
+#include <wchar.h>
+#include <io.h>
+#include <fcntl.h>
 
 using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
+    _setmode(_fileno(stdout), _O_U16TEXT);
     float pound;
     float shilling;
     int penny;
-    cout << "Введите число десятичных фунтов: "; cin >> pound;
+    wcout << L"Введите число десятичных фунтов: "; cin >> pound;
     shilling = pound * 20;
     penny = shilling * 12;
     shilling = (int)(penny / 12);
     penny = penny - shilling * 12;
     pound = (int)(shilling / 20);
     shilling = shilling - pound * 20;
-    cout << "Эквивалентная сумма в старой форме записи: " << (int)pound << "." << (int)shilling << "." << penny << "\n \n";
+    wchar_t F = L'\u00A3';
+    wcout << L"Эквивалентная сумма в старой форме записи: " << F << (int)pound << "." << (int)shilling << "." << penny << "\n \n";
     return 0;
 }
--- Lab_work_1/Еxercise_4/Еxercise_4/Еxercise_4.cpp
+++ Lab_work_1/Еxercise_4/Еxercise_4/Еxercise_4.cpp
@@ -1,17 +1,20 @@
 ﻿#include <iostream>
-
+#include <wchar.h>
+#include <io.h>
+#include <fcntl.h>
 using namespace std;
 
 int main()
 {
-    setlocale(LC_ALL, "Russian");
+    _setmode(_fileno(stdout), _O_U16TEXT);
     int pound;
     int shilling;
     int penny;
-    cout << "Введите количество фунтов: "; cin >> pound;
-    cout << "Введите количество шиллингов: "; cin >> shilling;
-    cout << "Введите количество пенсов: "; cin >> penny;
+    wcout << L"Введите количество фунтов: "; cin >> pound;
+    wcout << L"Введите количество шиллингов: "; cin >> shilling;
+    wcout << L"Введите количество пенсов: "; cin >> penny;
     penny = penny + (shilling * 12);
-    cout << "Десятичных фунтов: " << round((float)(pound + ( ((float)penny / 12) / 20 ))*100)/100 << "\n \n";
+    wchar_t F = L'\u00A3';
+    wcout << L"Десятичных фунтов: " << F << round((float)(pound + (((float)penny / 12) / 20)) * 100) / 100 << "\n \n";
     return 0;
 }
