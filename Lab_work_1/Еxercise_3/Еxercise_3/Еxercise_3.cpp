30c6b90a16e13fc76407ef599fba2fde2d051e24
Star_killer <68467056+Star-Kuller@users.noreply.github.com>
2022-09-24 12:13:51 +0300

    8

--- Lab_work_1/Еxercise_3/Еxercise_3/Еxercise_3.cpp
+++ Lab_work_1/Еxercise_3/Еxercise_3/Еxercise_3.cpp
@@ -5,10 +5,33 @@ using namespace std;
 int main()
 {
     setlocale(LC_ALL, "Russian");
-    int a,b,c,d;
+    int a,b,c,d, answ_up, answ_down, nod;
     char dummychar;
     cout << "Введите первую дробь: "; cin >> a >> dummychar >> b;
     cout << "Введите вторую дробь: "; cin >> c >> dummychar >> d;
-    cout << "Сумма равна: " << (a * d + b * c) << "/" << (b * d) << "\n \n";
+    // сложение
+    answ_up = (a * d + b * c);
+    answ_down = (b * d);
+    // сокращение
+    if (answ_up < answ_down) {
+        for (int i = 1; i < answ_down; i++) {
+            if ((answ_up % i == 0) & (answ_down % i == 0)) { // нахождение НОД, если знаменатель больше числителя
+                nod = i;
+            }
+        }
+    }
+    else {
+        for (int i = 1; i < answ_up; i++) {
+            if ((answ_up % i==0) & (answ_down % i==0)) { // нахождение НОД, если числитель больше знаменателя
+                nod = i;
+            }
+        }
+    }
+    if (answ_up % answ_down == 0) { // проверка является ли число целым
+        cout << "Сумма равна: " << answ_up / nod << "\n \n"; // вывод целого числа, если целое
+    }
+    else {
+        cout << "Сумма равна: " << answ_up / nod << "/" << answ_down / nod << "\n \n"; // вывод дробного числа, если дробное
+    }
     return 0;
 }
